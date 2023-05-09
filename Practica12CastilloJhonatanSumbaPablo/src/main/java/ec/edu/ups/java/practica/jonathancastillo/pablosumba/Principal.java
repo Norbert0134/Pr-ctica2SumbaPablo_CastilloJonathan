33037b13394be2cd49c061f4e331e6664ef5401a
PabloSumba <psumbag@est.ups.edu.ec>
2023-05-09 10:42:30 -0500

    PracticaN2

---
+++ Practica12CastilloJhonatanSumbaPablo/src/main/java/ec/edu/ups/java/practica/jonathancastillo/pablosumba/Principal.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package ec.edu.ups.java.practica.jonathancastillo.pablosumba;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Principal {
+    
+}
